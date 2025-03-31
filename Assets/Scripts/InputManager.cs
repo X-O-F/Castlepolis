6603459daaaa0d6b6a999233e38a5a3f741018a3
X-O-F <118107990+X-O-F@users.noreply.github.com>
2025-03-31 13:01:48 +0300

    Working on player sprinting

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,18 +3,20 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     public static Vector2 Movement;
-
+    public static bool isSprinting;
     private PlayerInput _playerInput;
     private InputAction _moveAction;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private InputAction _sprintAction;
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
         _moveAction = _playerInput.actions["Move"];
+        _sprintAction = _playerInput.actions["Sprint"];
     }
 
     private void Update()
     {
         Movement = _moveAction.ReadValue<Vector2>();
+        isSprinting = _sprintAction.IsPressed();
     }
 }
